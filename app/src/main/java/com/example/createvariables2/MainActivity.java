8d307cb105310f9e45a65c33e45d92d93fdd9bdf
ofeklevii <ofeklevi@sisma.org.il>
2024-01-02 15:45:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/createvariables2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.createvariables2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    int sum=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+    }
+
+    public void exe2(View view) {
+        sum++;
+        String str = sum + "";
+        btn.setText("This is a click number:" + str);
+    }
+}
